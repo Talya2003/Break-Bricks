e3f32420c984065f941427995ea13b566dadd8a9
Talya Kazayof <106309652+Talya2003@users.noreply.github.com>
2023-11-26 17:14:42 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -5,11 +5,12 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setBounds(10 , 10 , 700 , 600);
-        frame.setTitle("Brick Bracker");
+        frame.setTitle("Brick breaker");
         frame.setResizable(false);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //adding the game board to the frame we created
         GamePlay game_play = new GamePlay();
         frame.add(game_play);
     }
