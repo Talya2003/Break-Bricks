eef8ad233665453ac4a3a712893b3b62d902ce01
Talya Kazayof <106309652+Talya2003@users.noreply.github.com>
2023-11-26 08:11:08 +0200

    GamePlay.java

--- GamePlay.java
+++ GamePlay.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -22,11 +23,56 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+        time =  new Timer(delay , this);
+        time.start();
+    }
+
+    public void paint(Graphics g) {
+        //draw background
+        g.setColor(Color.black);
+        g.fillRect(1 , 1 , 692 , 592);
+
+        //draw borders
+        g.setColor(Color.black);
+        g.fillRect(0 , 0, 3 , 592);
+        g.fillRect(0 , 0, 692 , 3);
+        g.fillRect(692 , 0 , 3 , 592);
+
+        //the bottom
+        g.setColor(Color.green);
+        g.fillRect(player_x , 550 , 100 , 8);
+
+        //the ball
+        g.setColor(Color.yellow);
+        g.fillOval(ball_position_x , ball_position_y , 20 , 20);
+
+        g.dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        time.start();
 
+        if (is_play) {
+            if (new Rectangle(ball_position_x , ball_position_y , 20 , 20).intersects(new Rectangle(player_x , 555, 100 , 8))) {
+                ball_direction_y = -ball_direction_y;
+            }
+
+            ball_position_x += ball_direction_x;
+            ball_position_y += ball_direction_y;
+            if (ball_position_x < 0) {
+                ball_direction_x = -ball_direction_x;
+            }
+
+            if (ball_position_y < 0) {
+                ball_direction_y = -ball_direction_y;
+            }
+
+            if (ball_position_x > 670) {
+                ball_direction_x = -ball_direction_x;
+            }
+        }
+        repaint();
     }
 
     @Override
@@ -36,12 +82,40 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (player_x >= 600) {
+                player_x = 600;
+            }
+            else {
+                move_right();
+            }
+        }
 
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (player_x < 10) {
+                player_x = 10;
+            }
+            else {
+                move_left();
+            }
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+
+    public void move_right() {
+        is_play = true;
+        player_x += 20;
+    }
+
+
+    public void move_left() {
+        is_play = true;
+        player_x -= 20;
     }
 
 }
